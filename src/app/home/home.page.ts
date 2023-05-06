2ef2a32982b557650748400d80bba648132ca58b
brunasirne08 <101608588+brunasirne08@users.noreply.github.com>
2023-05-06 01:41:08 +0000

    0505

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalizacaoService } from '../services/localizacao.service';
+import { Produto } from '../services/produto.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  public listaProdutos: Produto[] = [];
 
-  constructor() { }
+  constructor(
+    private localizacaoService: LocalizacaoService) { }
 
   ngOnInit() {
-  }
 
+    setInterval(() => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position);
+        this.localizacaoService.inserir({
+          idusuario: 17,
+          longitude: position.coords.longitude,
+          latitude: position.coords.latitude,
+          horario: new Date(),
+          nome: "Bruna"
+        }).subscribe();
+      })
+    }, 2000);
+  }
 }
